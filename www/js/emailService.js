1c3de4d35b91d503d7b633afb089ad705f1e210f
jeffleus <jeffleus@gmail.com>
2016-02-12 12:09:49 -0800

    update email to attach receipt images
    add utility function to loop trip receipts and return an array of image
    file URI's for all the receipts that can be concat'd to the pdf file of
    the trip report.

--- www/js/emailService.js
+++ www/js/emailService.js
@@ -10,15 +10,18 @@ angular.module('starter.services')
             subj += ": " + moment(t.startDate).format("M-D-YY");
             subj += (t.endDate)?" - " + moment(t.endDate).format("M-D-YY"):"";
 
-            var b = 'Attached is a report template for testing email delivery of reimbursement reports.  The details of teh trip are provided in a tabular pdf file.  All supporting receipt documentation is missing for now with the next step to add them as attachments.\n\n';
+            var b = 'Attached is a reimbursement report and associated receipt images.  Trip details are summarized in the pdf file.  All supporting receipt documentation is attached as a separate image file for each receipt.\n\n';
             b += "Purpose: " + t.purpose;
+            
+            var attachments = [ file ];
+            attachments = attachments.concat(_receiptArray(t));
             
             var email = {
                 to: 'expensereport@athletics.ucla.edu', 
 				cc: 'akitagawa@athletics.ucla.edu',
 				subject: subj,
 				body: b, 
-				attachments: [ file ]
+				attachments: attachments
 			};
 
 			return $cordovaEmailComposer.open(email).catch(function(error) {
@@ -31,4 +34,16 @@ angular.module('starter.services')
 			console.log('trouble with the email composer availability.');
 		});
 	}
+    
+    function _receiptArray(t) {
+        var images = [];
+        if (t && t.receipts) {
+            t.receipts.forEach(function(r) {
+                var dir = cordova.file.documentsDirectory;
+                console.log(dir + r.image);
+                images.push(dir + r.image);
+            });
+        }
+        return images;
+    }
 });
