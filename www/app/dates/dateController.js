b6d835da233594b475998942782d9356fe301ed4
Jeff Leininger <jeffleus@gmail.com>
2016-06-16 11:07:46 -0700

    Revert "edit travel date per diems module"
    This reverts commit fe0e0f701e3747d47912d5a2ca7b206728ae0e77.

--- www/app/dates/dateController.js
+++ www/app/dates/dateController.js
@@ -10,7 +10,25 @@ angular.module('starter.controllers')
     $scope.deleteTravelDate = _deleteTravelDate;
     $scope.closeModal = _closeModal;
     //log the controller instantiation
-    console.log('DatesCtrl Controller... ');
+    console.log('DatesCtrl Controller...');
+//MODAL EVENTS
+    $scope.$on('modal.shown', function() {
+        if (!$scope.newTravelDate) {
+            $scope.newTravelDate = new TravelDate();
+        }
+    });
+	//Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+		// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+		// Execute action
+	});        
 				           
     function _saveTravelDate() {
         console.log('DatesCtrl: save new travel date');
@@ -38,7 +56,12 @@ angular.module('starter.controllers')
 	function _addTravelDate() {
 		_showTravelDateModal();
 	}
-//MODAL LOAD AND CLOSE				
+
+    function _closeModal() {
+		$scope.newTravelDate = null;
+		$scope.modal.hide();
+	};
+				
 	function _loadTravelDateModal() {
 		$ionicModal.fromTemplateUrl('app/dates/travelDateModal.html', {
 			scope: $scope,
@@ -48,27 +71,4 @@ angular.module('starter.controllers')
 			modal.show();
 		});
 	}
-
-    function _closeModal() {
-		$scope.newTravelDate = null;
-		$scope.modal.hide();
-	};
-//MODAL EVENTS
-    $scope.$on('modal.shown', function() {
-        if (!$scope.newTravelDate) {
-            $scope.newTravelDate = new TravelDate();
-        }
-    });
-	//Cleanup the modal when we're done with it!
-	$scope.$on('$destroy', function() {
-		$scope.modal.remove();
-	});
-	// Execute action on hide modal
-	$scope.$on('modal.hidden', function() {
-		// Execute action
-	});
-	// Execute action on remove modal
-	$scope.$on('modal.removed', function() {
-		// Execute action
-	});        
 });
